7c8dd27e044303aa0b9019900c7d9f2b32fd17b2
gurjeetkaur2811 <kgurjeet149@gmail.com>
2025-01-12 20:01:34 +0530

    gurjeet design

--- tri-secure/src/App.jsx
+++ tri-secure/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
    <h2>gurjeet</h2>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos fugit pariatur maxime officia earum voluptatum quisquam explicabo, esse aperiam harum. Minima optio assumenda nobis fuga rem maiores dolorem ipsum laboriosam, quam mollitia! Aliquam laudantium laboriosam ratione et eius totam! Ipsum!</p>
     </>
   )
 }
