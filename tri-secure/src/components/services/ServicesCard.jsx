c08d687c9f73eb10ee66ba2601140061cf02edcb
VanshitaKakkar19 <vanshitakakkar194@gmail.com>
2025-01-27 16:44:08 +0530

    Flip of service card

--- tri-secure/src/components/services/ServicesCard.jsx
+++ tri-secure/src/components/services/ServicesCard.jsx
@@ -1,43 +1,49 @@
 import React, { useState } from 'react';
 import { CButton, CCard, CCardBody, CCardImage, CCardText, CCardTitle } from '@coreui/react';
 import './ServicesCard.css';
+import ReactCardFlip from 'react-card-flip';
 
-export const ServicesCard = ({ service }) => {
-  const [isFlipped, setIsFlipped] = useState(false);
-
-  const handleFlip = () => {
-    setIsFlipped(!isFlipped);
-  };
-
+export const ServicesCard = ({ service, isFlipped, onFlip }) => {
   return (
-    <CCard className={`service-card ${isFlipped ? 'flipped' : ''}`}>
-     
-        {/* Front of the card */}
-        
-          {!isFlipped && (
-            <div className="service-card-image-wrapper">
-              <CCardImage src={service.image} />
-            </div>
-          )}
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      {/* Front of the card */}
+      <CCard className="service-card" key="front">
+        <div className="service-card-image-wrapper">
+          <CCardImage src={service.image} />
+        </div>
+        <CCardBody className="service-card-body">
+          <CCardTitle className="service-card-title">{service.name}</CCardTitle>
+          <CCardText className="service-card-text">{service.description}</CCardText>
+          <CButton
+            className="service-card-button"
+            color="primary"
+            onClick={onFlip} // Flip the card
+          >
+            View More
+          </CButton>
+        </CCardBody>
+      </CCard>
 
-          <CCardBody className="service-card-body">
-            <CCardTitle className="service-card-title">{service.name}</CCardTitle>
-            <CCardText className="service-card-text">
-              {isFlipped ? (
-                <ul>
-                  {service.points.map((point, index) => (
-                    <li key={index}>{point}</li>
-                  ))}
-                </ul>
-              ) : (
-                service.description
-              )}
-            </CCardText>
-            <CButton className="service-card-button" color="primary" onClick={handleFlip}>
-              {isFlipped ? 'View Less' : 'View More'}
-            </CButton>
-          </CCardBody>
-    
-    </CCard>
+      {/* Back of the card */}
+      <CCard className="service-card" key="back">
+        <CCardBody className="service-card-body">
+          <CCardTitle className="service-card-title">{service.name}</CCardTitle>
+          <div className="service-card-text">
+            <ul>
+              {service.points.map((point, index) => (
+                <li key={index}>{point}</li>
+              ))}
+            </ul>
+          </div>
+          <CButton
+            className="service-card-button"
+            color="primary"
+            onClick={onFlip} // Flip the card back
+          >
+            View Less
+          </CButton>
+        </CCardBody>
+      </CCard>
+    </ReactCardFlip>
   );
 };
